0358738062121f0da98e2e23661b854b1b924984
Ivan Stasevich <pravitel2015ify@gmail.com>
2023-12-02 14:15:25 +0300

    Update auth web app

--- src/web/auth.js
+++ src/web/auth.js
@@ -9,6 +9,12 @@ tg.MainButton.color = '#2cab37';
 let passwordInput = document.getElementById("password");
 let submitBtn = document.getElementById("submitBtn");
 
+submitBtn.addEventListener("click", function(e) {
+	e.preventDefault();
+	tg.MainButton.show();
+});
+
+
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	if (passwordInput.value) {
 		tg.sendData(passwordInput.value);
