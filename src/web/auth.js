ba0ab1e73bc35e607e7fc03777d376dce00418b0
Ivan Stasevich <pravitel2015ify@gmail.com>
2023-12-02 15:00:50 +0300

    Update auth web app

--- src/web/auth.js
+++ src/web/auth.js
@@ -1,34 +1,31 @@
-var tg = window.Telegram.WebApp;
+document.addEventListener("DOMContentLoaded", function() {
+    var tg = window.Telegram.WebApp;
 
-// Расширяем на весь экран.
-tg.expand();
+    // Расширяем на весь экран.
+    tg.expand();
 
-tg.MainButton.textColor = '#FFFFFF';
-tg.MainButton.color = '#2cab37';
-tg.MainButton.setText("Войти");
-tg.MainButton.show();
+    tg.MainButton.textColor = '#FFFFFF';
+    tg.MainButton.color = '#2cab37';
 
-let passwordInput = document.getElementById("password");
-let submitBtn = document.getElementById("submitBtn");
+    let passwordInput = document.getElementById("password");
+    let submitBtn = document.getElementById("submitBtn");
 
-submitBtn.addEventListener("click", function(e) {
-	e.preventDefault();
-	console.log("I work!!!");
-	submitBtn.value = "CLICKED!";
-	tg.MainButton.setText("Войти");
-    tg.MainButton.show();
+    submitBtn.addEventListener("click", function(e) {
+        e.preventDefault();
+        tg.MainButton.setText("Войти");
+        tg.MainButton.show();
+    });
+
+    Telegram.WebApp.onEvent("mainButtonClicked", function(){
+        if (passwordInput.value) {
+            tg.sendData(passwordInput.value);
+            passwordInput.value = '';
+        }
+    });
+
+    let user_card = document.getElementById("user_card");
+    let p = document.createElement("p");
+    p.innerText = `${tg.initDataUnsafe.user.first_name}
+    ${tg.initDataUnsafe.user.last_name}`;
+    user_card.appendChild(p);
 });
-
-
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	if (passwordInput.value) {
-		tg.sendData(passwordInput.value);
-		passwordInput.value = '';
-	}
-});
-
-let user_card = document.getElementById("user_card");
-let p = document.createElement("p");
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
-user_card.appendChild(p);
