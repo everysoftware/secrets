f91540af41545e7340fb5cc02959cd941104b95e
Ivan Stasevich <pravitel2015ify@gmail.com>
2024-10-02 23:54:59 +0300

    samesite = "none"

--- app/auth/transports.py
+++ app/auth/transports.py
@@ -8,8 +8,7 @@ from fastapi_users.authentication import (
 from app.config import settings
 
 cookie_transport = CookieTransport(
-    cookie_max_age=settings.app.auth_token_lifetime,
-    cookie_secure=False,
+    cookie_max_age=settings.app.auth_token_lifetime, cookie_samesite="none"
 )
 
 transports: Sequence[Transport] = [cookie_transport]
